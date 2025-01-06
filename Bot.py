293bdca0fd800fd08f96ed94af5eecf4d92bf089
ethanyeany <162551794+ethanyeany@users.noreply.github.com>
2025-01-06 01:56:05 -0500

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,14 +1,8 @@
 #Created by Ethan Yeany
 #12/26/24
 
-import time
 from NbaData import NbaData
 
-
-while True:
-
-    #Sleeps for one day in order to wait for nba.com to update
-    obj = NbaData()
-    obj.tweetGen()
-    time.sleep(86400)
-   
+obj = NbaData()
+obj.tweetGen()
+    
